9a59c1a371f7a8fdf367f1b9acc6bc7df7172f49
WayneLin1992 <62940642+WayneLin1992@users.noreply.github.com>
2021-04-19 17:12:00 +0800

    Add fiber, fiber.h and makesure to be right
    problem: the result didn't sequential
    Cause: the main thread didn't wait child thread
    solution: add the futex to makesure the answer to be sequential

--- fiber.c
+++ fiber.c
@@ -1,120 +1,107 @@
-#define FIBER_NOERROR 0
-#define FIBER_MAXFIBERS 1
-#define FIBER_MALLOC_ERROR 2
-#define FIBER_CLONE_ERROR 3
-#define FIBER_INFIBER 4
-
-/* The maximum number of fibers that can be active at once. */
-#define MAX_FIBERS 10
-
-/* The size of the stack for each fiber. */
-#define FIBER_STACK (1024 * 1024)
-
+#include "fiber.h"
 #define _GNU_SOURCE
-
-#include <sched.h> /* For clone */
 #include <stdio.h>
+#include <errno.h>
+#include <stdatomic.h>
+#include <stdint.h>
 #include <stdlib.h>
-#include <sys/types.h> /* For pid_t */
-#include <sys/wait.h>  /* For wait */
-#include <unistd.h>    /* For getpid */
-
-typedef struct {
-    pid_t pid;   /* The pid of the child thread as returned by clone */
-    void *stack; /* The stack pointer */
-} fiber_t;
-
-/* The fiber "queue" */
-static fiber_t fiber_list[MAX_FIBERS];
-
-/* The pid of the parent process */
-static pid_t parent;
-
-/* The number of active fibers */
-static int num_fibers = 0;
-
-void fiber_init()
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/syscall.h>
+#include <linux/futex.h>
+#include <sys/time.h>
+#define errExit(msg)    do { perror(msg); exit(EXIT_FAILURE); \
+                           } while (0)
+uint32_t futex_addr = 1;
+uint32_t futex_addr_1 = 1;
+futex(uint32_t *uaddr, int futex_op, uint32_t val, const struct timespec *timeout, uint32_t *uaddr2, uint32_t val3)
 {
-    for (int i = 0; i < MAX_FIBERS; ++i)
-        fiber_list[i].pid = 0, fiber_list[i].stack = 0;
-    parent = getpid();
+    return syscall(SYS_futex, uaddr, futex_op, val, timeout, uaddr2, val3);
 }
 
-/* Yield control to another execution context */
-void fiber_yield()
+static void fwait(uint32_t *futexp)
 {
-    /* move the current process to the end of the process queue. */
-    sched_yield();
+    long s;
+
+           /* atomic_compare_exchange_strong(ptr, oldval, newval)
+              atomically performs the equivalent of:
+
+                  if (*ptr == *oldval)
+                      *ptr = newval;
+
+              It returns true if the test yielded true and *ptr was updated. */
+
+    while (1) {
+
+               /* Is the futex available? */
+               const uint32_t one = 1;
+         if (atomic_compare_exchange_strong(futexp, &one, 0))
+             break;      /* Yes */
+
+               /* Futex is not available; wait. */
+
+         s = futex(futexp, FUTEX_WAIT, 0, NULL, NULL, 0);
+         if (s == -1 && errno != EAGAIN)
+             errExit("futex-FUTEX_WAIT");
+    }
 }
 
-struct fiber_args {
-    void (*func)(void);
-};
+       /* Release the futex pointed to by 'futexp': if the futex currently
+          has the value 0, set its value to 1 and the wake any futex waiters,
+          so that if the peer is blocked in fwait(), it can proceed. */
 
-static int fiber_start(void *arg)
+static void fpost(uint32_t *futexp)
 {
-    struct fiber_args *args = (struct fiber_args *) arg;
-    void (*func)() = args->func;
-    free(args);
+    long s;
+    /* atomic_compare_exchange_strong() was described
+         in comments above. */
+    const uint32_t zero = 0;
+    if (atomic_compare_exchange_strong(futexp, &zero, 1)) {
+        s = futex(futexp, FUTEX_WAKE, 1, NULL, NULL, 0);
+        if (s  == -1)
+            errExit("futex-FUTEX_WAKE");
+    }
+}
 
-    func();
+static void fibonacci() {
+    int fib[2] = {0, 1};
+    printf("Fib(0) = 0\nFib(1) = 1\n");
+    for (int i = 2; i < 15; ++i) {
+        int nextFib = fib[0] + fib[1];
+//        fwait(&futex_addr);
+	futex(&futex_addr, FUTEX_WAIT, 0, NULL, NULL, 0);
+        printf("Fib(%d) = %d\n", i, nextFib);;
+        fib[0] = fib[1];
+        fib[1] = nextFib;
+        futex(&futex_addr, FUTEX_WAKE, 1, NULL, NULL, 0);
+//        fpost(&futex_addr);
+        fiber_yield();
+    }
+}
+
+static void squares() {
+    for (int i = 1; i < 10; ++i) {
+        futex(&futex_addr_1, FUTEX_WAIT, 0, NULL, NULL, 0);
+        printf("%d * %d = %d\n", i, i, i * i);
+	futex(&futex_addr_1, FUTEX_WAKE, 1, NULL, NULL, 0);
+//        fpost(&futex_addr_1);
+//        futex_addr = 0;
+//        futex_wait(&futex_addr,0);
+        fiber_yield();
+    }
+}
+
+int main() {
+    fiber_init();
+
+    fiber_spawn(&fibonacci);
+    sleep(1);
+    fiber_spawn(&squares);
+
+    /* Since fibers are non-preemptive, we must allow them to run */
+//    sleep(1);
+    fiber_wait_all();
+
     return 0;
-}
-
-/* Creates a new fiber, running the func that is passed as an argument. */
-int fiber_spawn(void (*func)(void))
-{
-    if (num_fibers == MAX_FIBERS)
-        return FIBER_MAXFIBERS;
-
-    if ((fiber_list[num_fibers].stack = malloc(FIBER_STACK)) == 0)
-        return FIBER_MALLOC_ERROR;
-
-    struct fiber_args *args;
-    if ((args = malloc(sizeof(*args))) == 0) {
-        free(fiber_list[num_fibers].stack);
-        return FIBER_MALLOC_ERROR;
-    }
-    args->func = func;
-
-    fiber_list[num_fibers].pid = clone(
-        fiber_start, KKK,
-        SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, args);
-    if (fiber_list[num_fibers].pid == -1) {
-        free(fiber_list[num_fibers].stack);
-        free(args);
-        return FIBER_CLONE_ERROR;
-    }
-
-    num_fibers++;
-    return FIBER_NOERROR;
-}
-
-/* Execute the fibers until they all quit. */
-int fiber_wait_all()
-{
-    /* Check to see if we are in a fiber, since we do not get signals in the
-     * child threads
-     */
-    pid_t pid = getpid();
-    if (pid != parent)
-        return FIBER_INFIBER;
-
-    /* Wait for the fibers to quit, then free the stacks */
-    while (num_fibers > 0) {
-        if ((pid = wait(0)) == -1)
-            exit(1);
-
-        /* Find the fiber, free the stack, and swap it with the last one */
-        for (int i = 0; i < num_fibers; ++i) {
-            if (CCC) {
-                free(fiber_list[i].stack);
-                if (i != --num_fibers)
-                    fiber_list[i] = fiber_list[num_fibers];
-                break;
-            }
-        }
-    }
-
-    return FIBER_NOERROR;
 }
